6e044a1720aac667bf7bd2f54350964a667b4f4a
kinyuykelly <kinyuykelly@gmail.com>
2023-12-30 19:11:04 +0100

    javascript objects vs arrays

--- Week3/lesson7/src/index.js
+++ Week3/lesson7/src/index.js
@@ -3,7 +3,7 @@ let paris = {
   country: "France",
   language: "French",
   temperature: 18,
-  capitalCity: true
+  capitalCity: true,
 };
 console.log(paris.temperature);
 let sydney = {
@@ -11,36 +11,36 @@ let sydney = {
   country: "England",
   language: "English",
   temperature: 28,
-  capitalCity: false
+  capitalCity: false,
 };
 console.log(sydney.temperature);
 let cities = ["Newcastle", "Tamworth", "Hobart"];
 console.log(cities[0]);
 console.log(cities[1]);
 console.log(cities[2]);
-cities = [ newcastle ={
-  name: "Newcastle",
-  country: "France",
-  language: "French",
-  temperature: 11,
-  capitalCity: false
-},
-{
- name: "Tamworth",
-  country: "France",
-  language: "French",
-  temperature: 9,
-  capitalCity: false
-},
-{
-  name: "Hobart",
-  country: "France",
-  language: "French",
-  temperature: 10,
-  capitalCity: false
-}
+cities = [
+  {
+    name: "Newcastle",
+    country: "France",
+    language: "French",
+    temperature: 11,
+    capitalCity: false,
+  },
+  {
+    name: "Tamworth",
+    country: "France",
+    language: "French",
+    temperature: 9,
+    capitalCity: false,
+  },
+  {
+    name: "Hobart",
+    country: "France",
+    language: "French",
+    temperature: 10,
+    capitalCity: false,
+  },
 ];
 console.log(cities[0].temperature);
 console.log(cities[1].temperature);
 console.log(cities[2].temperature);
-
