a7fc6b3c316293242ff6d9222c2ef2e37517f871
kinyuykelly <kinyuykelly@gmail.com>
2024-01-06 21:25:00 +0100

    javascript selectors

---
+++ Week4/lesson7/src/index.js
@@ -0,0 +1,15 @@
+// Challenge 1
+// Log li with id special
+let li = document.querySelector("li.country#special");
+console.log(li);
+// Challenge 2
+// Log all li with class of country
+let lis = document.querySelectorAll(".country");
+console.log(lis)
+// Challenge 3
+// Add class special to the li with id special
+li.classList.add("");
+// Challenge 4
+// In your code, replace the content of the element with id special
+// by your favorite country (other than Portugal of course)
+ li.innerHTML = "Spain🇪🇸";
