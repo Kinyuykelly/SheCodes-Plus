cb88c226543a0205ecaf0bed73517e8d49b8b397
kinyuykelly <kinyuykelly@gmail.com>
2024-01-07 20:36:47 +0100

    javascript events

--- Week4/lesson8/src/index.js
+++ Week4/lesson8/src/index.js
@@ -1,15 +1,15 @@
 // Challenge 1
 // Add click eventListener to the button. Alert `It is 18 degrees`
+let message = document.querySelector("button");
+message.addEventListener("click", display, label);
 function display(){
     alert("It is 18 degrees")
 }
 display();
-let message = document.querySelector("button");
-message.addEventListener("click", display);
 // Challenge 2
 // When the button is clicked, replace button label by ‘18 degrees’
 function label(){
-    message.innerHTML = "18 degrees"
+    message.innerText = "18 degrees"
 }
 label();
-message.addEventListener("click", label);
+
