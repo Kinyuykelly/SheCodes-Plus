01ebf472757604e82c24602d6217e1d303205dae
kinyuykelly <kinyuykelly213@gmail.com>
2024-01-11 21:44:03 +0100

    javascript forms

--- Week4/lesson9/src/index.js
+++ Week4/lesson9/src/index.js
@@ -2,13 +2,13 @@ let clickMe = document.querySelector("#special-button");
      clickMe.addEventListener("click", function(){
         alert("Hooray");
      });
-let submitform = document.querySelector("#submitbtn");
-submitform.addEventListener("click", function(){
+let submitform = document.querySelector("#password-form");
+submitform.addEventListener("submit", function(){
     let password = document.querySelector("#password-input").value;
     alert(password);
 });
-let submit = document.querySelector("#btnsubmit");
-submit.addEventListener("click", function(){
+let submit = document.querySelector("#signup-form");
+submit.addEventListener("submit", function(){
     let email = document.querySelector("#email-input").value;
     let username = document.querySelector("#username-input").value;
     alert(`${email} and ${username}`);
