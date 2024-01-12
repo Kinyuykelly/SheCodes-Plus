85e2e764a3ceecb2a917a8669ebe36282649f2a8
kinyuykelly <kinyuykelly213@gmail.com>
2024-01-12 18:06:55 +0100

    weather api

--- Week5/lesson4/src/index.js
+++ Week5/lesson4/src/index.js
@@ -1,7 +1,6 @@
 let city = "Sydney"
 let apiKey = "a235423bf4aefc61ac00dafo8c073tb3";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
-`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 function displayTemperature(response){
     let heading = document.querySelector("h1");
     let city = response.data.city;
